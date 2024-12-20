05a2d779c848b373a2b61a1cee9ecc289fa778b3
Sandali <it21114144@my.sliit.lk>
2024-12-20 18:34:35 +0530

    last commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import '../styles/styles.css';
+import logo from '../images/logo.png';
+
+function Footer(){
+    const footerStyle = {
+        fontFamily: 'Lato, sans-serif'
+    };
+
+    const techStyle = {
+        fontFamily: 'Inter, sans-serif'
+    }
+
+    return(
+        <div className='footer'>
+            <div className='footerbody'>
+                <div classname='companyabout'>
+                    <img src={logo} alt='' className='logo2'/>
+
+                    <div className='about' style={footerStyle}>
+                        Your goal is our target. Not anything in between. We use 
+                        online marketing platforms and tools to achieve 
+                        single objective - your business results.
+                    </div>
+                </div>
+                <div className='technologydet'>
+                    <div className='title' style={techStyle}>
+                        Our Technologies
+                    </div>
+                    <div className='technologies' style={techStyle}>
+                        <ul>
+                            <li> ReactJS </li>
+                            <li> Gatsby </li>
+                            <li> NextJS </li>
+                            <li> NodeJS </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='servicedet'>
+                    <div className='title'>
+                        Our Services
+                    </div>
+                    <div className='service' style={techStyle}>
+                        <ul>
+                            <li> Social media Marketing </li>
+                            <li> Web & Mobile App Development </li>
+                            <li> Data & Analytics </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+            <center>
+                <hr/>
+                <div className='policy'style={techStyle}>
+                    <span> Privacy Policy </span> | <span> Terms & Conditions </span> 
+                </div>
+            </center>
+        </div>
+    )
+}
+
+export default Footer;
